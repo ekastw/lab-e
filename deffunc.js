8902b469cb99e1f4d61cf2462faa05ca12c73383
ekastw <66845486+ekastw@users.noreply.github.com>
2020-06-15 08:42:31 +0700

    Update deffunc.js

--- deffunc.js
+++ deffunc.js
@@ -1729,7 +1729,7 @@ function after_submit(form_name,data){
 		}
 	}
   if (form_name=="payment_check_out") {
-    var inv = $('[name="check_out_transaction"]').attr("inv-num");
+    var inv = $('[name="payment_check_out"]').attr("inv-num");
     if (data=='1' || data=='11') {
       location.href = base_url+'keranjang?inv='+inv+'&finish'
     }
